5c099954e9c192e38dfba3f2bab6f49186748b96
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-05-16 15:08:18 -0500

    Configurando conexion, crear crud con asistente

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        //NOTACIONES - DATA NOTATION
+        [Display(Name="País")]
+        [MaxLength(50 , ErrorMessage = "El campo es {0} debe tener maximo {1} caracterés")]
+        [Required(ErrorMessage = "El campo es {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
